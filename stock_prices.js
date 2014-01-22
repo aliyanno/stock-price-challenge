7df0e17a968e21dcf3212bc93ac2be48b98d46ea
aliyanno <aliyamarder@gmail.com>
2014-01-21 21:29:09 -0800

    changes made after code review

--- stock_prices.js
+++ stock_prices.js
@@ -1,13 +1,9 @@
 //script to calculate the best times to buy/sell a stock to maximize profit
 //start with an integer list of prices sorted by date
 
-//check for basic case: absolute minimum before absolute maximum
-
 //Main Function
-
-function stockIndexFunction (array) {
-	var array = [3, 4, 6, 10, 20];
-	var value = getBuySellIndexes(array);
+function computeBestStockTransactions (stockPrices) {
+	var value = getBuySellIndexes(stockPrices);
 	if (value[0] == -1 && value[1] == -1) {
 		return "Sorry, nothing is profitable... damn Enron."
 	} else {
@@ -15,203 +11,226 @@ function stockIndexFunction (array) {
 	}
 };
 
+// Code Summary: Find the local minima of Stock Prices.
+// For each local minima, find the absolute maximum of the Stock
+// prices after each local minimum (since you must buy before you sell).
+// Use each local minimum and corresponding absolute maximum to 
+// find the profit margin for each pair.
+// Find the best possible profit margin.
+// Work backwards to find the index of the local minimum corresponding 
+// maximum with the best profit margin.
+// Return indexes of local minimum and absolute maximum.
 
+
+// Get an array of the greatest profit margins based on local minima and local maxima
+function getRelativeProfitValues (stockPrices) {
+	var profitMargins = [],
+		localMinIndex = getMinIndexes(stockPrices),
+		minValues = getMinValues(stockPrices, localMinIndex),
+		maxValues = getMaxValues(stockPrices, localMinIndex);
+	for (var i = 0; i < minValues.length; i++) {
+		var profit = maxValues[i] - minValues[i];
+		profitMargins.push(profit);
+	};
+	return profitMargins;
+};
+
+// Find the index of absolute max in an array in order to find the best profit margin
 function getAbsoluteMaxIndex (array) {
-	var absMax = Math.max.apply( Math, array );
-	var indexMax = array.indexOf(absMax);
+	var absMax = Math.max.apply(Math, array),
+		indexMax = array.indexOf(absMax);
 	return indexMax;
 };
 
-function getAbsoluteMinIndex (array) {
-	var absMin = Math.min.apply( Math, array );
-	indexMin = array.indexOf(absMin);
-	return indexMin;
-};
-
-function compareAbsolute (array, indexMax, indexMin) {
-	var absDifference = (indexMax - indexMin);
-	if (absDifference > 0) {
-		return [indexMin, indexMax];
-	} else {
-		getBuySellIndexes(array);
-	};
-};
-
-//run absolute Solution test
-
-function getAbsoluteSolution (array) {
-	var indexMax = getAbsoluteMaxIndex(array);
-	var indexMin = getAbsoluteMinIndex(array);
-	var transactionArray = compareAbsolute(array, indexMax, indexMin);
-
-	return transactionArray;
-};
-
-//find local profit spans based on local minimum 
-//and absolute maximum in remainder of array from local min.
-
-//create an array of indexes of local mins
-
-function getMinIndexes (array) {
-	var localMinArray = [];
-	if (array[0] < array [1]) {
-		localMinArray.push(0);
-	};
-	for (var i = 1; i < array.length; i++) {
-		if (array[i-1] > array[i] && array[i] < array[i+1]) {
-			localMinArray.push(i);
-		};
-	};
-	return localMinArray;
-};
-//TEST
-function testLocalMinIndex () {
-	var array = [10, 200, 25, 30, 15, 5, 1, 30, 50, 100, 2];
-	var value = getMinIndexes(array);
-	if (value[0] == 0 && value[1] == 4 && value[2] == 9 && value[3] == 14) {
-		return true;
-	} else {
-		return value;
-	};
-};
-
-//get array of maximum values after each local minimum
-
-function getMaxValues (array) {
-	var maxValueArray = [],
-		remainder = getMinIndexes(array);
-	for (var i = 0; i < remainder.length; i++) {
-		var newArray = array.slice(remainder[i]);
-		var maxIndex = getAbsoluteMaxIndex(newArray);
-		var relativeMax = newArray[maxIndex];
-		maxValueArray.push(relativeMax);
-	};
-	return maxValueArray;
-};
-
-
-//TEST
-
-function testGetMaxValues () {
-	var array = [10, 200, 25, 30, 15, 5, 1, 30, 50, 100, 2];
-	var value = getMaxValues(array);
-	return value;
-};
-
-//get max values indexes from values found above
-
-function getMaxIndexes (array) {
-	var maxValues = getMaxValues(array);
-	var maxValueIndex = [];
-	for (var i = 0; i < maxValues.length; i++) {
-		var maxIndex = array.indexOf(maxValues[i]);
-		maxValueIndex.push(maxIndex);
-	};
-	return maxValueIndex;
-};
-
-//TEST
-
-function testGetMaxIndexes () {
-	var array = [10, 200, 25, 30, 15, 5, 1, 30, 50, 100, 2];
-	var value = getMaxIndexes(array);
-	return value;
-};
-
-//get relative minimum values
-
-function getMinValues (array) {
-	var minimumValues = []
-	var remainder = getMinIndexes(array);
-	for (var i = 0; i < remainder.length; i++) {
-		var minIndex = remainder[i];
-		var minValue = array[minIndex];
-		minimumValues.push(minValue);
-	};
-	return minimumValues;
-};
-
-//TEST
-
-function testGetMinValues () {
-	var array = [10, 200, 25, 30, 15, 5, 1, 30, 50, 100, 2];
-	var value = getMinValues(array)
-	return value;
-};
-
-//compare relative minimum values and corresponding maximum values
-
-function compareRelativeProfit (array) {
-	var profitMargin = [];
-	var minValues = getMinValues(array);
-	var maxValues = getMaxValues(array);
-	for (var i = 0; i < minValues.length; i++) {
-		var profit = maxValues[i] - minValues[i];
-		profitMargin.push(profit);
-	};
-	return profitMargin;
-};
-
-//TEST
-
-function testCompareRelativeProfit () {
-	var array = [10, 200, 25, 30, 15, 5, 1, 30, 50, 100, 2];
-	var value = compareRelativeProfit(array);
-	return value;
-};
-
-//get the index of the greatest profit margin
-
-function getProfitIndex (array) {
-	var profitMargin = compareRelativeProfit(array),
-		profitValue = Math.max.apply (Math, profitMargin),
-		profitIndex = profitMargin.indexOf(profitValue);
+// Optimal profit index of the relative profit margins from local minima and maxima
+function getOptimalProfitIndex (stockPrices) {
+	var profitMargin = getRelativeProfitValues(stockPrices),
+		profitIndex = getAbsoluteMaxIndex(profitMargin);
 	return profitIndex;
 };
 
-//find the correlating values for the largest profit margin
+// Find the optimal Buy and Sell point based on the optimal Profit difference
+function getBuySellIndexes (stockPrices) {
+	var profitIndex = getOptimalProfitIndex(stockPrices);
 
-function getBuySellIndexes (array) {
-	var profitIndex = getProfitIndex(array);
-
-	var minIndexes = getMinIndexes(array),
-		maxIndexes = getMaxIndexes(array);
+	var minIndexes = getMinIndexes(stockPrices),
+		maxIndexes = getMaxIndexes(stockPrices, minIndexes);
 
 	var minOptimalIndex = minIndexes[profitIndex],
 		maxOptimalIndex = maxIndexes[profitIndex];
 
-	var minOptimalValue = array[minOptimalIndex],
-		maxOptimalValue = array[maxOptimalIndex];
+	var minOptimalValue = stockPrices[minOptimalIndex],
+		maxOptimalValue = stockPrices[maxOptimalIndex];
 
-	var buyOptimal = array.indexOf(minOptimalValue),
-		sellOptimal = array.indexOf(maxOptimalValue),
+	var buyOptimal = stockPrices.indexOf(minOptimalValue),
+		sellOptimal = stockPrices.indexOf(maxOptimalValue),
 		buySellIndex = [buyOptimal, sellOptimal];
 
 	return buySellIndex;
 };
 
-//Test
-function testGetBuySellIndexes (array) {
-	var array = [10, 200, 25, 30, 15, 5, 1, 30, 50, 100, 2];
-	var value = getBuySellIndexes(array);
-	return value;	
-}
+// Functions for Local Minima and corresponding Maxima
 
-//tests
+// Get the local minima of the Stock Prices to determine any possible Buy points
+function getMinIndexes (stockPrices) {
+	var localMinIndex = [];
+	// Check if first price index is a local minimum
+	if (stockPrices[0] < stockPrices [1]) {
+		localMinIndex.push(0);
+	};
+	// Find local minima by comparing with price before and after
+	// Ignore last price for local minima because you cannot sell stock if bought there
+	for (var i = 1; i < stockPrices.length - 1; i++) {
+		if (stockPrices[i-1] > stockPrices[i] && stockPrices[i] < stockPrices[i+1]) {
+			localMinIndex.push(i);
+		};
+	};
+	return localMinIndex;
+};
 
-function testAbsolutesolution () {
+// Get Max Indexes in the Stock Prices Array from the values found in getMaxValues
+// in order to determine the best sell index
+function getMaxIndexes (stockPrices, localMinIndex) {
+	var maxValues = getMaxValues(stockPrices, localMinIndex),
+		maxValueIndex = [];
+	for (var i = 0; i < maxValues.length; i++) {
+		var maxIndex = stockPrices.indexOf(maxValues[i]);
+		maxValueIndex.push(maxIndex);
+	};
+	return maxValueIndex;
+};
+
+// Get the absolute maximum of the sub array starting with each local minimum
+// to determine the greatest profit margin from that minimum
+function getMaxValues (stockPrices, localMinIndex) {
+	//Get the local minimum indexes of Stock Prices
+	var maxValueArray = [];
+	for (var i = 0; i < localMinIndex.length; i++) {
+		// Create a sub array of Stock Prices to find maximum after minimum
+		var remainder = stockPrices.slice(localMinIndex[i]);
+		// Get absolute max index of sub array
+		var maxIndex = getAbsoluteMaxIndex(remainder);
+		// Get absolute max value and push to Max Values Array
+		var relativeMax = remainder[maxIndex];
+		maxValueArray.push(relativeMax);
+	};
+	return maxValueArray;
+};
+
+// Get the values of the local minima to calculate profit margins for each pair of local minima and maxima
+function getMinValues (stockPrices, localMinIndex) {
+	var minimumValues = [];
+	for (var i = 0; i < localMinIndex.length; i++) {
+		var minIndex = localMinIndex[i],
+			minValue = stockPrices[minIndex];
+		minimumValues.push(minValue);
+	};
+	return minimumValues;
+};
+
+
+
+
+//
+//
+//
+//
+//
+
+//Tests for individual functions
+function masterTester () {
+	testLocalMinIndex();
+	testGetMaxValues();
+	testAbsoluteMaxIndex();
+	testGetMaxIndexes();
+	testGetMinValues();
+	testGetRelativeProfitValues();
+	testGetBuySellIndexes();
+};
+
+function testAbsoluteMaxIndex () {
+	console.log("Testing AbsoluteMaxIndex");
 	var array = [10, 200, 25, 30, 15, 5, 1, 30, 50, 100, 2];
-	var value = getabsoluteSolution (array);
-	if (value[0] == 4 && value[1] == 9) {
+	var value = getAbsoluteMaxIndex(array);
+	if (value == 1) {
 		return true;
 	} else {
-		return value;
+		console.log("AbsoluteMaxIndex failed");
+		console.log(value);
 	};
 };
 
-function testAbsoluteMax () {
+function testLocalMinIndex () {
+	console.log("Testing LocalMinIndex");
 	var array = [10, 200, 25, 30, 15, 5, 1, 30, 50, 100, 2];
-	var value = getAbsoluteMaxIndex(array);
-	return value;
+	var value = getMinIndexes(array);
+	if (value[0] == 0 && value[1] == 2 && value[2] == 6) {
+		return true;
+	} else {
+		console.log("LocalMinIndex Failed");
+		console.log(value);
+	};
+};
 
-}
+function testGetMaxValues () {
+	console.log("Testing GetMaxValues");
+	var array = [10, 200, 25, 30, 15, 5, 1, 30, 50, 100, 2];
+	var localMinIndex = [0, 2, 6];
+	var value = getMaxValues(array, localMinIndex);
+	if (value[0] == 200 && value[1] == 100 && value[2] == 100) {
+		return true;
+	} else {
+		console.log("GetMaxValues Failed");
+		console.log(value);
+	};
+};
+
+function testGetMaxIndexes () {
+	console.log("Testing getMaxIndexes");
+	var array = [10, 200, 25, 30, 15, 5, 1, 30, 50, 100, 2],
+		localMinIndex = [0, 2, 6];
+	var value = getMaxIndexes(array, localMinIndex);
+	if (value[0] == 1 && value[1] == 9 && value[2] == 9) {
+		return true;
+	} else {
+		console.log("getMaxIndexes Failed");
+		console.log(value);
+	}
+};
+
+function testGetMinValues () {
+	console.log("Testing getMinValues");
+	var array = [10, 200, 25, 30, 15, 5, 1, 30, 50, 100, 2],
+		localMinIndex = [0, 2, 6];
+	var value = getMinValues(array, localMinIndex)
+	if (value[0] == 10 && value[1] == 25 && value[2] == 1) {
+		return true;
+	} else {
+		console.log("getMinValues Failed");
+		console.log(values);
+	};
+};
+
+function testGetRelativeProfitValues () {
+	console.log("Testing getRelativeProfitValues");
+	var array = [10, 200, 25, 30, 15, 5, 1, 30, 50, 100, 2];
+	var value = getRelativeProfitValues(array);
+	if (value[0] == 190 && value[1] == 75 && value[2] == 99) {
+		return true;
+	} else {
+		console.log("getRelativeProfitValues failed");
+		console.log(values);
+	};
+};
+
+function testGetBuySellIndexes (array) {
+	console.log("Testing getBuySellIndexes");
+	var array = [10, 200, 25, 30, 15, 5, 1, 30, 50, 100, 2];
+	var value = getBuySellIndexes(array);
+	if (value[0] == 0 && value[1] == 1) {
+		return true;
+	} else {
+		console.log("getBuySellIndexes Failed");
+		console.log(values);
+	};
+};
